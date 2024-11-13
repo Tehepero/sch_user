0aeb8e76a6cb5be56070de7aa912478d7ac5ab34
jinsang park <jsgd1254@gmail.com>
2024-11-13 09:02:53 +0900

    init

---
+++ src/main/java/com/sparta/sch_user/SchUserApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.sch_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchUserApplication.class, args);
+    }
+
+}
