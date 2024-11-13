c042a9e89bcc932b5140b33469ba989637b8ed43
jinsang park <jsgd1254@gmail.com>
2024-11-13 10:00:57 +0900

    Lv.4 개발
    - 로그인 개발
    - 로그인 하지 않은 경우 일정 관련 기능은 사용할 수 없도록 개발

--- src/main/java/com/sparta/sch_user/service/UserService.java
+++ src/main/java/com/sparta/sch_user/service/UserService.java
@@ -1,15 +1,20 @@
 package com.sparta.sch_user.service;
 
+import com.sparta.sch_user.dto.LoginRequestDto;
 import com.sparta.sch_user.dto.UserRequestDto;
 import com.sparta.sch_user.dto.UserResponseDto;
 import com.sparta.sch_user.entity.User;
 import com.sparta.sch_user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -39,5 +44,13 @@ public class UserService {
 
     public User findUserById(Long id) {
         return userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+    }
+
+    public User loginUser(LoginRequestDto loginRequestDto) {
+        User user = userRepository.findByEmail(loginRequestDto.getEmail());
+        if (user == null || !Objects.equals(user.getPassword(), loginRequestDto.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "유효하지 않은 사용자 이름 혹은 잘못된 비밀번호");
+        }
+        return user;
     }
 }
